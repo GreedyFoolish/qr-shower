ae4211d2c4a9651e1d2f1832e772d03dae9b1a9c
GreedyFoolish <1158984036@qq.com>
2025-09-19 10:07:28 +0800

    Initial commit

---
+++ README.js
@@ -0,0 +1,11 @@
+// git 初始化
+// git init
+// npm 初始化
+// npm init -y
+// 添加 .gitignore 文件，内容如下：
+// node_modules
+// ,DS_Store
+// 安装依赖
+// npm i webpack
+// 添加远程仓库
+// git remote add origin https://github.com/GreedyFoolish/qr-shower.git
